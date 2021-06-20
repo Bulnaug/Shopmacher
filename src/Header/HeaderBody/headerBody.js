3e5d940c75bef44d2eb58d4ed41563e5f2c1d95f
Bulnaug <57222348+Bulnaug@users.noreply.github.com>
2021-06-20 19:39:04 +0300

    header update
    the header has been updated

--- src/Header/HeaderBody/headerBody.js
+++ src/Header/HeaderBody/headerBody.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import style from './headerBody.module.css'
 
@@ -24,11 +23,11 @@ const HeaderBody = () => {
           </div>
           <div className={style.sub_nav}>
             <ul>
-              <li><a href="/"><span className={style.icon}><FontAwesomeIcon icon={faCoffee} /></span><span>Service</span></a></li>
-              <li><a href="/damen"><span className={style.icon}><FontAwesomeIcon icon={faCoffee} /></span><span>Geschäftskunden</span></a></li>
-              <li><a href="/herren"><span className={style.icon}><FontAwesomeIcon icon={faCoffee} /></span><span>Merkzettel</span></a></li>
-              <li><a href="#"><span className={style.icon}><FontAwesomeIcon icon={faCoffee} /></span><span>Konto</span></a></li>
-              <li><a href="#"><span className={style.icon}><FontAwesomeIcon icon={faCoffee} /></span><span>Warenkorb</span></a></li>
+              <li><a href="/"><span className={style.icon} data-icon='service'></span><span>Service</span></a></li>
+              <li><a href="/damen"><span className={style.icon} data-icon='customers'></span><span>Geschäftskunden</span></a></li>
+              <li><a href="/herren"><span className={style.icon} data-icon='heart'></span><span>Merkzettel</span></a></li>
+              <li><a href="#"><span className={style.icon} data-icon='account'></span><span>Konto</span></a></li>
+              <li><a href="#"><span className={style.icon} data-icon='cart'></span><span>Warenkorb</span></a></li>
             </ul>
           </div>
         </div>
